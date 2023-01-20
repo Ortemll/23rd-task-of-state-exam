42b2ef7c4801763f3b7739fb40756547867ef889
Ortemll <74818248+Ortemll@users.noreply.github.com>
2023-01-20 11:55:08 +0300

    Update 23.py
    Kinda sorta debugged it

--- 23.py
+++ 23.py
@@ -13,13 +13,13 @@ for i in range(len(TO_WALK_THROUGH)):
 _intervals += [(prev_point, END)]
 for inter in _intervals:
     values = [[i, 1] if i == inter[1] else [i, 0] for i in range(inter[0], inter[1]+1)]
-    for i in range(len(values)-1, 0, -1):
+    for i in range(len(values)-2, -1, -1):
         val = values[i][0]
         if val in NOT_TO_WALK_THROUGH: continue
         for command in COMMANDS:
             new_val = command[0](val)
-            if new_val in range(inter[0], inter[1]+1):
-                values[val][1] += values[new_val-inter[0]][1]
+            if new_val <= inter[1]:
+                values[i][1] += values[new_val-inter[0]][1]
     _mapped_intervals.append(values)
             # TODO: IMPLEMENT COUNTING THE USAGE OF A COMMAND
 result = 1
